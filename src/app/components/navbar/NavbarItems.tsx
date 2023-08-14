9dbb9edef05c61b0cdc4beff1c4b094a05221305
Felix Pacheco <devfex.19@gmail.com>
2023-08-14 19:28:30 -0400

    Update navbar

--- src/app/components/navbar/NavbarItems.tsx
+++ src/app/components/navbar/NavbarItems.tsx
@@ -4,13 +4,10 @@ export const NavbarItems = () => {
     return (
         <>
             <li className="hover:underline">
-                <Link href="/about">Acerca De</Link>
+                <Link href="/services">Servicios</Link>
             </li>
             <li className="hover:underline">
                 <Link href="/projects">Proyectos</Link>
-            </li>
-            <li className="hover:underline">
-                <Link href="/services">Servicios</Link>
             </li>
             <li className="hover:underline">
                 <Link href="/contact">Contacto</Link>
