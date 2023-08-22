d3beba4b7c86c46e58d09f5ec3b1b47f223da4f9
Felix Pacheco <devfex.19@gmail.com>
2023-08-22 19:52:36 -0400

    Finish integration of embla-carousel

--- src/app/components/carousel/Layer.tsx
+++ src/app/components/carousel/Layer.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
-import { EProjectsCategory } from "src/types";
 
 interface IProps {
     title: string,
-    category: EProjectsCategory
     src: string,
 }
 
-export const Layer = ({ title, category, src }: IProps) => {
+export const Layer = ({ title, src }: IProps) => {
     return (
         <>
             <Image fill className="object-cover" src={src} alt={title} />
-            <div className="bg-dravys-gray-700 absolute top-0 right-0 left-0 bottom-0 bg-opacity-50" />
-            <h4 className="uppercase text-xl tracking-widest absolute top-0 p-10">{title}</h4>
-            <span className="italic inline-block absolute -rotate-90 bottom-36 xl:bottom-48 -left-16 tracking-[1rem] text-xs uppercase">
-                {category}
-            </span>
+            <div className="bg-dravys-gray-700 absolute hover:bg-opacity-0 top-0 right-0 left-0 bottom-0 bg-opacity-50">
+                <div className="relative w-full h-full">
+                    <span className="italic inline-block absolute bottom-5 right-5 text-lg tracking-widest uppercase font-extralight">
+                        {title}
+                    </span>
+                </div>
+            </div>
         </>
     );
 };
--- src/app/components/home/Projects.tsx
+++ src/app/components/home/Projects.tsx
@@ -1,91 +1,88 @@
 import { SectionTitle } from "@/components/SectionTitle";
-import { Carousel } from "@/components/carousel/Carousel";
 import { Layer } from "@/components/carousel/Layer";
 import { EProjectsCategory } from "@/types/index";
 import { renderDriveImage } from "@/utils/renderDriveImage";
+import { Carousel } from "../carousel/Carousel";
 
 const example = [
     {
-        title: 'Santa Fe',
-        category: EProjectsCategory.bathrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1NNQtOvry0U_vyizVTMV3gIuj26b8DNIV/view?usp=drive_link')
-    },
-    {
-        title: 'Santa Fe',
-        category: EProjectsCategory.bathrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1j19c_kqMwtXmbqVl7uPLJ2zCSb3iURFw/view?usp=drive_link')
-
-    },
-    {
-        title: 'Santa Fe',
-        category: EProjectsCategory.bathrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1IGR38RekYTSZu88LOuONPaHxRHwlg62X/view?usp=drive_link')
-
-    },
-    {
-        title: 'Santa Fe',
-        category: EProjectsCategory.bathrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1D3zaJ8F2B63dcjt3FtHfq_Cn4QapTkfj/view?usp=drive_link')
-
-    },
-    {
         title: 'La Florida',
-        category: EProjectsCategory.bedrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1DcAQ8OgLUoz7a4yqqZLpTOCyPIpJIO5T/view?usp=drive_link')
+        src: renderDriveImage('https://drive.google.com/file/d/1p6646M4KG_3BeyMhBLe30xNVacyD3IpH/view?usp=drive_link')
+    },
+    {
+        title: 'Santa Paula',
+        category: EProjectsCategory.bathrooms,
+        src: renderDriveImage('https://drive.google.com/file/d/1po6SsgDwqzM_xEQsgKMmauFUkD8w3IBq/view?usp=drive_link')
 
     },
     {
-        title: 'La Florida',
-        category: EProjectsCategory.bedrooms,
+        title: 'Santa Rosa',
+        src: renderDriveImage('https://drive.google.com/file/d/1khRftHtFvUvDmKjplB1oPrYXTvaVz_kl/view?usp=drive_link')
+
+    },
+    {
+        title: 'OF. Vestier',
+        src: renderDriveImage('https://drive.google.com/file/d/1K3BALlYC5zPuzQsxD7xgIFfMdftljmeg/view?usp=drive_link')
+
+    },
+    {
+        title: 'Santa Fe',
         src: renderDriveImage('https://drive.google.com/file/d/1nOa39YnSfhE1LLHDWcmM3qpXZdnTHZ51/view?usp=drive_link')
 
     },
     {
-        title: 'La Florida',
-        category: EProjectsCategory.bedrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1kOBa3lR2QMHHitsWXcroMfmDLN1QE4P5/view?usp=drive_link')
+        title: 'Quinta Irene',
+        src: renderDriveImage('https://drive.google.com/file/d/1XvQNZOoVCjXAEJk8ENiI3ag1THCz3sfC/view?usp=drive_link')
 
     },
     {
-        title: 'La Florida',
-        category: EProjectsCategory.kitchens,
-        src: renderDriveImage('https://drive.google.com/file/d/1eWqETthnK_4AK9x_QG3aSVADK-KMHqVe/view?usp=drive_link')
+        title: 'Sebucan',
+        src: renderDriveImage('https://drive.google.com/file/d/12tf_okZ7i85J5O_VIRnodlzvyfk6gDno/view?usp=drive_link')
     },
     {
-        title: 'La Florida',
-        category: EProjectsCategory.kitchens,
-        src: renderDriveImage('https://drive.google.com/file/d/12LaQWo0_i0Zx5TKXGlA4b9C966fNU5g2/view?usp=drive_link')
-    },
-    {
-        title: 'Las Mercedes',
-        category: EProjectsCategory.kitchens,
-        src: renderDriveImage('https://drive.google.com/file/d/1N1YV7noju5z38kD2ulNrY878H8tZOVy_/view?usp=drive_link')
-    },
-    {
-        title: 'El hatillo',
-        category: EProjectsCategory.architecture,
-        src: renderDriveImage('https://drive.google.com/file/d/1VysQuVuUH7GYDZCrMTzteukdiJ583g7c/view?usp=drive_link')
-    },
-    {
-        title: 'G5 Projects',
-        category: EProjectsCategory.bedrooms,
-        src: renderDriveImage('https://drive.google.com/file/d/1jgjDe8XlCapSTU-35Q3Yg1aZFtdOyNSC/view?usp=drive_link')
+        title: 'Sebucan',
+        src: renderDriveImage('https://drive.google.com/file/d/1K3BALlYC5zPuzQsxD7xgIFfMdftljmeg/view?usp=drive_link')
     }
+    // {
+    //     title: 'La Florida',
+    //     category: EProjectsCategory.kitchens,
+    //     src: renderDriveImage('https://drive.google.com/file/d/12LaQWo0_i0Zx5TKXGlA4b9C966fNU5g2/view?usp=drive_link')
+    // },
+    // {
+    //     title: 'Las Mercedes',
+    //     category: EProjectsCategory.kitchens,
+    //     src: renderDriveImage('https://drive.google.com/file/d/1N1YV7noju5z38kD2ulNrY878H8tZOVy_/view?usp=drive_link')
+    // },
+    // {
+    //     title: 'El hatillo',
+    //     category: EProjectsCategory.architecture,
+    //     src: renderDriveImage('https://drive.google.com/file/d/1VysQuVuUH7GYDZCrMTzteukdiJ583g7c/view?usp=drive_link')
+    // },
+    // {
+    //     title: 'G5 Projects',
+    //     category: EProjectsCategory.bedrooms,
+    //     src: renderDriveImage('https://drive.google.com/file/d/1jgjDe8XlCapSTU-35Q3Yg1aZFtdOyNSC/view?usp=drive_link')
+    // }
 ];
 
 export const Projects = () => {
     return (
-        <section className="text-center py-10">
+        <section className="text-center py-10 select-none">
             <SectionTitle title="Mis" complement="Proyectos" />
-            <div className="mt-10 text-isabelline-500 tracking-[.5rem] text-left overflow-hidden">
-                <Carousel loop>
-                    {example.map(({ category, src, title }) => {
+            <div className="mt-10 text-isabelline-500 overflow-hidden">
+                <Carousel
+                    loop
+                    dragFree={false}
+                    watchDrag={false}
+                    slidesToScroll={4}
+                    breakpoints={{
+                        '(max-width: 768px)': { slidesToScroll: 2 }
+                    }}
+                >
+                    {example.map(({ src, title }) => {
                         return (
-                            <div className="relative h-[400px] xl:h-[700px] flex-[0_0_100%]" key={src}>
-                                <Layer
-                                    title={title}
-                                    src={src}
-                                    category={category} />
+                            <div className="relative h-[400px] xl:h-[700px] w-full embla-slide" key={src}>
+                                <Layer title={title} src={src} />
                             </div>
                         );
                     })}
