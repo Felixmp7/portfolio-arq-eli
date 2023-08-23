cc154e176ac81856b8f7a7e09f9005bcc36d9f16
Felix Pacheco <devfex.19@gmail.com>
2023-08-23 11:32:32 -0400

    Update props of SectionTitle

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -1,11 +1,20 @@
+import clsx from "clsx";
+
 interface IProps {
     title: string
-    complement: string
+    complement?: string
+    className?: string
 }
-export const SectionTitle = ({ title, complement }: IProps) => {
+export const SectionTitle = ({ title, complement, className = '' }: IProps) => {
+    const classNames = clsx({
+        "text-4xl uppercase font-semibold tracking-[.5rem]": true,
+        [className]: !!className
+    });
     return (
-        <h2 className="text-4xl uppercase font-semibold tracking-[.5rem]">{title}
-            <span className="ml-6 text-redwood">{complement}</span>
+        <h2 className={classNames}>{title}
+            {complement && (
+                <span className="ml-6 text-redwood">{complement}</span>
+            )}
         </h2>
     );
 };
