6319eb1368475ea2f8db4aefe33ddaa7f5d97fa2
Felix Pacheco <devfex.19@gmail.com>
2023-08-24 16:45:40 -0400

    Ingore

--- src/app/hooks/useContactForm.ts
+++ src/app/hooks/useContactForm.ts
@@ -4,6 +4,34 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 // import { POST } from "@/api/send/route";
 
+// import { EmailTemplate } from '@/components/EmailTemplate';
+// import { TSendEmailForm } from '@/types/index';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST({ email, name, phone, company, message }: TSendEmailForm) {
+//     try {
+//         const data = await resend.emails.send({
+//             from: `${name} <${email}>`,
+//             to: ['eliana.guerrero.arq@gmail.com'],
+//             subject: `Contacto a través del portafolio - ${name}`,
+//             text: undefined as any,
+//             react: EmailTemplate({name,
+//                 email,
+//                 company,
+//                 phone,
+//                 message})
+//         });
+
+//         return NextResponse.json(data);
+//     } catch (error) {
+//         return NextResponse.json({ error });
+//     }
+// }
+
+
 const defaultValues:TSendEmailForm = {
     email: '',
     message: '',
