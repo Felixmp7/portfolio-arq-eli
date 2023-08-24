a0dbfd0c8fb233c01e60dbf2102b30427ea0487c
Felix Pacheco <devfex.19@gmail.com>
2023-08-24 15:05:58 -0400

    Allow subtitle to receive className attribute

--- src/app/components/Subtitle1.tsx
+++ src/app/components/Subtitle1.tsx
@@ -1,10 +1,16 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 interface IProps {
-    children: ReactNode
+    children: ReactNode,
+    className?: string
 }
-export const Subtitle1 = ({ children }: IProps) => {
+export const Subtitle1 = ({ children, className = '' }: IProps) => {
+    const classes = clsx({
+        'font-bold text-xl leading-6': true,
+        [className]: !!className
+    });
     return (
-        <h2 className="font-bold text-xl leading-6">{children}</h2>
+        <h2 className={classes}>{children}</h2>
     );
 };
