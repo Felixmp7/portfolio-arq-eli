c3dff4953488c62edcb6ea1eae974fc8a716db44
Felix Pacheco <devfex.19@gmail.com>
2023-08-24 15:06:07 -0400

    Implement Tab Component

---
+++ src/app/components/Tab.tsx
@@ -0,0 +1,23 @@
+import { ETabKeyIcon } from "@/types/index";
+import { GoGoal } from 'react-icons/go';
+import { MdCalendarMonth, MdLocationPin, MdOutlineTitle } from 'react-icons/md';
+
+type TProps = {
+    title: string,
+    keyIcon: ETabKeyIcon
+}
+
+export const Tab = ({title, keyIcon}: TProps) => {
+    const ICONS = {
+        [ETabKeyIcon.title]: <MdOutlineTitle />,
+        [ETabKeyIcon.location]: <MdLocationPin />,
+        [ETabKeyIcon.year]: <MdCalendarMonth />,
+        [ETabKeyIcon.goal]: <GoGoal />
+    };
+    return (
+        <span className="inline-flex bg-dravys-gray bg-opacity-40 items-center py-2 px-3 rounded-full text-isabelline gap-1 text-xs">
+            <span className="text-lg">{ICONS[keyIcon]}</span>
+            {title}
+        </span>
+    );
+};
