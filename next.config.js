48ab01600c713a2bd08c8c63e7d860655dd3eb75
Felix Pacheco <devfex.19@gmail.com>
2023-08-25 16:12:17 -0400

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['drive.google.com']
-        // remotePatterns: [
-        //     {
-        //         protocol: 'https',
-        //         hostname: 'drive.google.com'
-        //     }
-        // ]
+        domains: ['drive.google.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "*.googleusercontent.com",
+                pathname: "**"
+            }
+        ]
     }
 };
 
