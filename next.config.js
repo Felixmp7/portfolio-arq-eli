457678d66936786708b00a31b3450890e24e19ce
Felix Pacheco <devfex.19@gmail.com>
2023-08-25 09:04:49 -0400

    Change next config to check error with images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'drive.google.com'
-            }
-        ]
+        domains: ['drive.google.com']
+        // remotePatterns: [
+        //     {
+        //         protocol: 'https',
+        //         hostname: 'drive.google.com'
+        //     }
+        // ]
     }
 };
 
