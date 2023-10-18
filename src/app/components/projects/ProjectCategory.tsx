fa49c1c4c79d7ff62bfa11925d75082b1fd791ec
Felix Pacheco <devfex.19@gmail.com>
2023-10-18 15:52:24 -0400

    Fix layout of projects

--- src/app/components/projects/ProjectCategory.tsx
+++ src/app/components/projects/ProjectCategory.tsx
@@ -1,14 +1,6 @@
 import { SectionTitle } from "@/components/SectionTitle";
 import { ProjectItem } from "@/components/projects/ProjectItem";
 import { TProjectDetails } from "@/types/index";
-import clsx from "clsx";
-
-const getGridClassnames = (index: number, className = '') => clsx({
-    'relative': true,
-    'md:col-span-5': index === 0 || index === 3,
-    'md:col-span-3': index === 1 || index === 2,
-    [className]: !!className
-});
 
 type TProps = {
     title: string,
@@ -21,18 +13,19 @@ const ProjectCategory = ({ projects, title, complement }: TProps) => {
     return (
         <>
             <SectionTitle title={title} complement={complement} className="ml-10 mb-12" />
-            <div className="grid md:grid-cols-8 h-[800px] gap-1">
-                {noProjects ? Array.from(Array(4).keys()).map((item, index) => (
-                    <div key={item} className={getGridClassnames(index, 'bg-gray-300 animate-pulse')}  />
-                )) :projects.map(({ id, title, images }, index) => {
-                    if (index === 3) index = 0;
+            <ul className="grid md:grid-cols-8 gap-1 projects-layout" >
+                {noProjects ? Array.from(Array(8).keys()).map((item) => {
                     return (
-                        <div key={id} className={getGridClassnames(index)}>
+                        <li key={item} className='bg-gray-400 animate-pulse h-[400px]'  />
+                    );
+                }) :projects.map(({ id, title, images }) => {
+                    return (
+                        <li key={id}>
                             <ProjectItem title={title} src={images[0]} id={id} />
-                        </div>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
         </>
     );
 };
