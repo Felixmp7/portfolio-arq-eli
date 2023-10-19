062f2758f384a398d670e1eb05051a4c37a8922e
Felix Pacheco <devfex.19@gmail.com>
2023-10-19 10:41:37 -0400

    Update Services Grid

--- src/app/components/home/ServicesResume.tsx
+++ src/app/components/home/ServicesResume.tsx
@@ -7,7 +7,7 @@ export const ServicesResume = () => {
     return (
         <section className="text-center py-10 px-10 sm:px-20 lg:px-40">
             <SectionTitle title="Mis" complement="Servicios" />
-            <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-20 mt-12 text-left">
+            <div className="grid sm:grid-cols-1 xl:grid-cols-3 gap-10 sm:gap-20 mt-12 text-left">
                 <div className="text-7xl">
                     <Image
                         src="/assets/svg-icons/pencil-rule.svg"
@@ -42,22 +42,9 @@ export const ServicesResume = () => {
                         width={70}
                         height={70}
                     />
-                    <Subtitle2 subtitle="Modelado 3D" />
+                    <Subtitle2 subtitle="Modelado 3D y Renders" />
                     <p className="text-gray-500 leading-6 text-sm mt-3">
                         Modelamos el espacio que necesites, mediante planos 2D o imágenes de referencia.
-                    </p>
-                </div>
-                <div className="text-7xl">
-                    <Image
-                        src="/assets/svg-icons/renders.svg"
-                        alt="Buildings"
-                        className="mb-4"
-                        width={70}
-                        height={70}
-                    />
-                    <Subtitle2 subtitle="Renders" />
-                    <p className="text-gray-500 leading-6 text-sm mt-3">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                     </p>
                 </div>
             </div>
