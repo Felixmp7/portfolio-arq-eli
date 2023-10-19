f37f92dc8137a0dfb26c1c31a983560204ae1a2a
Felix Pacheco <devfex.19@gmail.com>
2023-10-19 10:41:12 -0400

    Return services sorted by id 'asc'

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -3,7 +3,11 @@ import { prisma } from 'prisma/client';
 
 export async function GET() {
     try {
-        const services: TService[] = await prisma.service.findMany();
+        const services: TService[] = await prisma.service.findMany({
+            orderBy: {
+                id: 'asc'
+            }
+        });
         return services;
     } catch (error) {
         return [];
