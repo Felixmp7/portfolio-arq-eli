af70ce9baa34d858ae9ce48b18632221ea35e148
Felix Pacheco <devfex.19@gmail.com>
2023-10-28 15:07:33 -0400

    Update links of social media

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -8,17 +8,17 @@ export const SocialMedia = () => {
     return (
         <ul className='inline-flex items-center text-2xl gap-x-3'>
             <li>
-                <Link href="https://www.linkedin.com/in/felix-pacheco-30/" target='_blank'>
+                <Link href="www.linkedin.com/in/eliana-guerrero-v" target='_blank'>
                     <IoLogoLinkedin />
                 </Link>
             </li>
             <li>
-                <Link href="https://github.com/Felixmp7" target='_blank'>
+                <Link href="https://www.instagram.com/elianagv_arq/" target='_blank'>
                     <IoLogoInstagram />
                 </Link>
             </li>
             <li>
-                <Link href="https://github.com/Felixmp7" target='_blank'>
+                <Link href="https://drive.google.com/drive/u/0/folders/1OI-_5oPJcomQ6drZMP453mJLX5IRXjp8" target='_blank'>
                     <SiGoogledrive />
                 </Link>
             </li>
