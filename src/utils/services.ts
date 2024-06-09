4fea54193c8951950465b1ac2109e1fcf84aabf7
Felix Pacheco <devfex.19@gmail.com>
2024-06-08 20:15:11 -0400

    Update file paths for services.json and projects.json

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -6,11 +6,11 @@ const parseFile = (file: string) => JSON.parse(file);
 const readFile = (path: string) => fs.readFile(process.cwd() + path, 'utf8');
 
 export const getProjects = async () => {
-    const file = await readFile('/.data/projects.json');
+    const file = await readFile('/src/.data/projects.json');
     return parseFile(file) as Project[];
 };
 
 export const getServices = async () => {
-    const file = await readFile('/.data/services.json');
+    const file = await readFile('/src/.data/services.json');
     return parseFile(file) as Service[];
 };
